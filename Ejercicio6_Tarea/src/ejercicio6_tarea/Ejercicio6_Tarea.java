998cea82014d1bf6ac1abd129ee543dff4f4ecc5
Juan Carlos Pizarro Alonso <127835047+juancarlospizarro@users.noreply.github.com>
2024-10-08 18:43:26 +0200

    Ejercicio 6 y 7

--- Ejercicio6_Tarea/src/ejercicio6_tarea/Ejercicio6_Tarea.java
+++ Ejercicio6_Tarea/src/ejercicio6_tarea/Ejercicio6_Tarea.java
@@ -13,11 +13,14 @@ public class Ejercicio6_Tarea {
         int suma = 0;
         int i;
         System.out.println("SUMA RANGO N");
+        // Solicitamos el número al usuario
         System.out.println("Por favor, introduzca el número: ");
         n = sc.nextInt();
+        // Sumamos todos los números desde 1 hasta el número y acumulamos la suma en la variable suma
         for(i = 1; i < n; i++){
             suma = suma + i;
         }
+        // Imprimimos por pantalla el resultado
         System.out.println("La suma de los números entre 1 y N es " + suma + ".");
     }
     
--- Ejercicio7_Tarea/src/ejercicio7_tarea/Ejercicio7_Tarea.java
+++ Ejercicio7_Tarea/src/ejercicio7_tarea/Ejercicio7_Tarea.java
@@ -11,14 +11,17 @@ public class Ejercicio7_Tarea {
         int número;
         int mul;
         System.out.println("CALCULAR TABLA DE MULTIPLICAR");
+        // Solicitamos al usuario un número entre 1 y 10
         System.out.println("Por favor, introduzca el número entre 1 y 10: ");
         número = sc.nextInt();
+        // Comprobamos que el número sea válido
         while(número < 1 || número > 10){
             System.err.println("Número fuera de rango.");
             System.out.println("Por favor, introduzca un número valido (entre 1 y 10): ");
             número = sc.nextInt();
         }
         
+        // Calculamos y devolvemos por pantalla el resultado de cada multiplicación del número por cada número entre 0 y 10
         for(int i = 0; i <= 10; i++){
             mul = número * i;
             System.out.println(número + " x " + i + " = " + mul);
